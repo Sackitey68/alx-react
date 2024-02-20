b0a86ab0d8f4f7dd93dcdc14f190dff93eabc2ac
Isaac Sackey Sackitey <sackitey68@yahoo.com>
2024-02-20 03:16:56 +0000

    Update App.test.js

--- 0x05-react_state/task_0/dashboard/src/App/App.test.js
+++ 0x05-react_state/task_0/dashboard/src/App/App.test.js
@@ -9,6 +9,14 @@ import Footer from "../Footer/Footer";
 import Notifications from "../Notifications/Notifications";
 import CourseList from "../CourseList/CourseList";
 import { shallow, mount } from "enzyme";
+import { StyleSheetTestUtils } from "aphrodite";
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
 
 describe("App tests", () => {
   it("renders without crashing", () => {
@@ -85,4 +93,35 @@ describe("When ctrl + h is pressed", () => {
     wrapper.unmount();
   });
   document.alert.mockClear();
+});
+
+it("Has default state for displayDrawer false", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.state().displayDrawer).toEqual(false);
+});
+
+it("displayDrawer changes to true when calling handleDisplayDrawer", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.state().displayDrawer).toEqual(false);
+
+  const instance = wrapper.instance();
+
+  instance.handleDisplayDrawer();
+
+  expect(wrapper.state().displayDrawer).toEqual(true);
+});
+
+it("displayDrawer changes to false when calling handleHideDrawer", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.state().displayDrawer).toEqual(false);
+
+  // const instance = wrapper.instance();
+
+  wrapper.instance().handleDisplayDrawer();
+
+  expect(wrapper.state().displayDrawer).toEqual(true);
+
+  wrapper.instance().handleHideDrawer();
+
+  expect(wrapper.state().displayDrawer).toEqual(false);
 });

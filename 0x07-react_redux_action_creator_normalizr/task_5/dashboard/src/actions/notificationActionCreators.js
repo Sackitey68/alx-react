58044d540efc2e1d2114ee5d373b7c80a854b7f8
Isaac Sackey Sackitey <sackitey68@yahoo.com>
2024-03-01 06:08:08 +0000

    Update notificationActionCreators.js

--- 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -1,15 +1,16 @@
 import { NotificationTypeFilters, MARK_AS_READ, SET_TYPE_FILTER } from "./notificationActionTypes";
 
-export const markAsRead = (index) => {
-  return {
-    type: MARK_AS_READ,
-    index,
-  };
-};
+import { markAsRead, setNotificationFilter } from "./notificationActionCreators";
 
-export const setNotificationFilter = (filter) => {
-  return {
-    type: SET_TYPE_FILTER,
-    filter,
-  };
-};
+describe("tests for notification actions", () => {
+  it("should create right action for mark as read", () => {
+    expect(markAsRead(1)).toEqual({ type: MARK_AS_READ, index: 1 });
+  });
+
+  it("should create right action for notification filter", () => {
+    expect(setNotificationFilter(NotificationTypeFilters["DEFAULT"])).toEqual({
+      type: SET_TYPE_FILTER,
+      filter: "DEFAULT",
+    });
+  });
+});

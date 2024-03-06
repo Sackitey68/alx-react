4ff077d6596e4567318fcb5aab61d8ae2bc96a86
Sackitey68 <sackitey68@gmail.com>
2024-03-06 10:58:24 +0000

    React Redux reducer+selector

--- 0x08-react_redux_reducer_selector/task_3/dashboard/src/actions/courseActionCreators.test.js
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/actions/courseActionCreators.test.js
@@ -1,11 +1,14 @@
-import { selectCourse, unSelectCourse } from "./courseActionCreators";
-import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
+import { selectCourse, unSelectCourse } from './courseActionCreators';
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
 
-describe("tests for action creators", () => {
-  it("should return right action payload and type when selectCourse is called", () => {
-    expect(selectCourse(1)).toEqual({ type: SELECT_COURSE, index: 1 });
+describe('action creators', () => {
+  it('selectCourse', () => {
+    const result = selectCourse(1);
+    expect(result).toEqual({ type: SELECT_COURSE, index: 1 });
   });
-  it("should return right action payload and type when unSelectCourse is called", () => {
-    expect(unSelectCourse(1)).toEqual({ type: UNSELECT_COURSE, index: 1 });
+
+  it('unSelectCourse', () => {
+    const result = unSelectCourse(1);
+    expect(result).toEqual({ type: UNSELECT_COURSE, index: 1 });
   });
 });

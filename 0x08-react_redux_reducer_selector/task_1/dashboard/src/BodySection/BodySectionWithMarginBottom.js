27a59783ddf7733f7025bbfd3ac9ab0866912305
Isaac Sackey Sackitey <sackitey68@yahoo.com>
2024-03-06 04:50:51 +0000

    React Redux reducer+selector

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,21 +1,34 @@
-import PropTypes from "prop-types";
-import React, { Component } from "react";
-import BodySection from "./BodySection";
-import "./BodySectionWithMarginBottom.css";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BodySection from './BodySection';
+import { StyleSheet, css } from 'aphrodite';
 
 class BodySectionWithMarginBottom extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div className="bodySectionWithMargin">
+      <div className={css(styles.bodySectionWithMargin)}>
         <BodySection {...this.props} />
       </div>
     );
   }
 }
 
-BodySectionWithMarginBottom.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+BodySectionWithMarginBottom.defaultProps = {
+  title: '',
 };
+
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string,
+};
+
+const styles = StyleSheet.create({
+  boySectionWithMargin: {
+    width: '100%',
+  },
+});
 
 export default BodySectionWithMarginBottom;

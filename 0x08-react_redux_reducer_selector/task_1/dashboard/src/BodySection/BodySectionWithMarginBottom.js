50690be810a0a433f80880fee8803bd33ae39a35
Sackitey68 <sackitey68@gmail.com>
2024-03-06 04:37:58 +0000

    React Redux reducer+selector

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,23 +1,17 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
 import BodySection from "./BodySection";
-import { StyleSheet, css } from "aphrodite";
+import "./BodySectionWithMarginBottom.css";
 
 class BodySectionWithMarginBottom extends Component {
   render() {
     return (
-      <div className={css(styles.bodySectionWithMargin)}>
+      <div className="bodySectionWithMargin">
         <BodySection {...this.props} />
       </div>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  bodySectionWithMargin: {
-    marginBottom: "40",
-  },
-});
 
 BodySectionWithMarginBottom.propTypes = {
   title: PropTypes.string.isRequired,
--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/schema/notifications.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/schema/notifications.js
@@ -1,17 +1,35 @@
-import * as notificationData from "../../../../notifications.json";
-import { normalize, schema } from "normalizr";
+import * as notificationsData from '../../notifications.json';
+import { normalize, schema } from 'normalizr';
 
-const user = new schema.Entity("users");
-const message = new schema.Entity("messages", {}, { idAttribute: "guid" });
-const notification = new schema.Entity("notification", {
+export const getAllNotificationsByUser = (userId) => {
+  const notifications = normalized.entities.notifications;
+  const messages = normalized.entities.messages;
+  const notificationsByUser = [];
+
+  for (const property in notifications) {
+    if (notifications[property].author === userId) {
+      notificationsByUser.push(messages[notifications[property].context]);
+    }
+  }
+
+  return notificationsByUser;
+};
+
+const user = new schema.Entity('users');
+
+const message = new schema.Entity(
+  'messages',
+  {},
+  {
+    idAttribute: 'guid',
+  }
+);
+
+const notification = new schema.Entity('notifications', {
   author: user,
   context: message,
 });
 
-const normalized = normalize(notificationData, [notification]);
-
-export default function getAllNotificationsByUser(userId) {
-  return notificationData.filter((notification) => notification.author.id === userId).map((notification) => notification.context);
-}
+const normalized = normalize(notificationsData.default, [notification]);
 
 export { normalized };

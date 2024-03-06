4d24e8d2c0540d4256a5cd7023452a21f03f7cfb
Isaac Sackey Sackitey <sackitey68@yahoo.com>
2024-03-06 05:00:18 +0000

    React Redux reducer+selector

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -1,20 +1,21 @@
+import { toJS } from 'immutable';
 import uiReducer, { initialState } from './uiReducer';
 import { DISPLAY_NOTIFICATION_DRAWER } from '../actions/uiActionTypes';
 
 describe('reducer', function () {
   it('initial state', function () {
     const state = uiReducer(undefined, {});
-    expect(state).toEqual(initialState);
+    expect(state.toJS()).toEqual(initialState);
   });
 
   it('SELECT_COURSE', function () {
     const state = uiReducer(undefined, { type: 'SELECT_COURSE' });
-    expect(state).toEqual(initialState);
+    expect(state.toJS()).toEqual(initialState);
   });
 
   it('DISPLAY_NOTIFICATION_DRAWER', function () {
     const state = uiReducer(undefined, { type: DISPLAY_NOTIFICATION_DRAWER });
-    expect(state).toEqual({
+    expect(state.toJS()).toEqual({
       ...initialState,
       isNotificationDrawerVisible: true,
     });

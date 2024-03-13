318739cc3cd6695d0dec10aeb3ae0d5b45de1831
Isaac Sackey Sackitey <sackitey68@yahoo.com>
2024-03-13 18:11:07 +0000

    react_redux_connectors_and_providers

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,21 +1,42 @@
-import PropTypes from "prop-types";
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import BodySection from "./BodySection";
-import "./BodySectionWithMarginBottom.css";
+import { StyleSheet, css } from "aphrodite";
 
 class BodySectionWithMarginBottom extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div className="bodySectionWithMargin">
+      <div className={css(styles.bodySectionWithMargin)}>
         <BodySection {...this.props} />
       </div>
     );
   }
 }
 
-BodySectionWithMarginBottom.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+BodySectionWithMarginBottom.defaultProps = {
+  title: "",
 };
+
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string,
+};
+
+const screenSize = {
+  small: "@media screen and (max-width: 900px)",
+};
+
+const styles = StyleSheet.create({
+  bodySectionWithMargin: {
+    marginBottom: "40px",
+    width: "100%",
+    [screenSize.small]: {
+      marginBottom: "10px",
+    },
+  },
+});
 
 export default BodySectionWithMarginBottom;
